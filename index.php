eeb5dc97b4a5fb78c5ff34435fb5d59e1ed7ecf2
Audrey Smith <audcsm@gmail.com>
2022-03-06 20:56:55 +0000

    Created index.php and committing for testing

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+$PageTitle="Advanced Obesity Surgery";
+
+include_once('/partials/header.php');
+
+?>
+
+<main>
+            <div id="banner-img" style="background-image:url(http://placekitten.com/1300/600)">
+                <div class="banner-overlay_navy"></div>
+                <div class="container over-banner">
+                    <h1 class="text_right">AdventHealth Celebration <br />Bariatric Surgery</h1>
+                    <h2>Based in Celebration, Central Florida</h2>
+                    <h4>Introductory information, marketing message, here.</h4>
+                    <a class="btn large">Start here <i class="bi bi-chevron-double-right"></i></a>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-lg-9">
+                        <h5>A few sentences of quick information, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus dui, cursus vitae elit quis, mattis suscipit massa. Fusce nec diam leo.</h5>
+                        <br />
+                    </div>
+                    <div class="col-sm-12 col-lg-3 centered">
+                        <a class="btn">Read more</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 col-lg-9">
+                        <h5>Proin quis sem elit. Nunc eget eros aliquet, lobortis ante sit amet, porttitor turpis. Proin sodales fringilla condimentum. Phasellus tristique sem et orci feugiat, ac euismod purus faucibus. Curabitur pretium ante at nisi accumsan, vitae iaculis lectus porttitor.</h5>
+                    </div>
+                    <div class="col-sm-12 col-lg-3 centered">
+                        <a class="btn">Meet Dr. Smith</a>
+                    </div>
+                </div>
+            </div>
+            <div class="feature-section">
+                <div class="row">
+                    <span id="pointer_left" class="col-sm-0 col-lg-3"></span>
+                    <div id="feature-text" class="col-sm-12 col-lg-6">
+                        <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus dui, cursus vitae elit quis, mattis suscipit massa. Fusce nec diam leo."</p></blockquote>
+                    </div>
+                    <span id="pointer_right" class="col-sm-0 col-lg-3"></span>
+                </div>
+            </div>
+            <div class="container centered">
+                <h5>Marketing message with something like “click below to take the first step” Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus dui, cursus vitae elit quis, mattis suscipit massa.</h5>
+                <a class="btn large">Start here <i class="bi bi-chevron-double-right"></i></a>
+            </div>
+        </main>
+        
+    </body>
+</html>
+
+<?php
+
+// include_once('/partials/footer.php');
+
+?>
---
+++ partials/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name ="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="stylesheet" href="../css/bs/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="../css/typography.css" />
+        <link rel="stylesheet" href="../css/colours.css" />
+        <link rel="stylesheet" href="../css/header.css" />
+        <link rel="stylesheet" href="../css/footer.css" />
+        <link rel="stylesheet" href="../css/core.css" />
+        
+        <script src="../scripts/bs/bootstrap.bundle.min.js"></script>
+        <script src="../scripts/jquery.js"></script>
+        <script src="../scripts/app.js"></script>
+        <script>
+        window.onscroll = function() {stickyHeader()};
+
+        var header = document.getElementById("header");
+        var sticky = header.offsetHeight;
+
+        function stickyHeader() {
+          if (window.pageYOffset >= sticky) {
+            header.classList.add("sticky")
+          } else {
+            header.classList.remove("sticky");
+          }
+        }
+        </script>
+    </head>
+    
+    <body>
+        <header id="header">
+            <div class="container">
+                <div class="logo">
+                </div>
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-heading">About</a>
+                        <ul class="dropdown">
+                            <li><a>Procedures</a></li>
+                            <li><a>The Hospital</a></li>
+                            <li><a>Meet Dr. Smith</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-heading">Resources</a>
+                        <ul class="dropdown">
+                            <li><a>Pre- and Post-op Resources</a></li>
+                            <li><a>Video Gallery</a></li>
+                            <li><a>Blog</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-heading">Start Here</a></li>
+                </ul>
+            </div>
+        </header>
