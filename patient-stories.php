0bf6ea46498bff7d2e6d6ab6235a74aeb528d518
Audrey Smith <audcsm@gmail.com>
2022-03-12 01:14:58 +0000

    Changed name of some pages for consistency

---
+++ patient-stories.php
@@ -0,0 +1,69 @@
+<?php
+
+$PageTitle="Advanced Obesity Surgery";
+
+include_once('./partials/header.php');
+
+?>
+
+        <main>
+            <div id="banner-img" style="background-image:url(http://placekitten.com/1300/600)">
+                <div class="banner-overlay_navy"></div>
+                <div class="container over-banner">
+                    <h1 class="text_right">Patient Experiences &#38;<br />Success Stories</h1>
+                </div>
+            </div>
+            <div class="container">
+                <div class="patient-review">
+                    <h5><strong>Name/Anon from City, State <br />Received X Operation</strong></h5>
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus dui, cursus vitae elit quis, mattis suscipit massa. Fusce nec diam leo."</blockquote>
+                </div>
+                <hr />
+                <div class="patient-review">
+                    <h5><strong>Name/Anon from City, State <br />Received X Operation</strong></h5>
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus dui, cursus vitae elit quis, mattis suscipit massa. Fusce nec diam leo."</blockquote>
+                </div>
+                <hr />
+                <div class="patient-review">
+                    <h5><strong>Name/Anon from City, State <br />Received X Operation</strong></h5>
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus dui, cursus vitae elit quis, mattis suscipit massa. Fusce nec diam leo."</blockquote>
+                </div>
+                <hr />
+                <div class="patient-review">
+                    <h5><strong>Name/Anon from City, State <br />Received X Operation</strong></h5>
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus dui, cursus vitae elit quis, mattis suscipit massa. Fusce nec diam leo."</blockquote>
+                </div>
+                <hr />
+                <div class="patient-review">
+                    <h5><strong>Name/Anon from City, State <br />Received X Operation</strong></h5>
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus dui, cursus vitae elit quis, mattis suscipit massa. Fusce nec diam leo."</blockquote>
+                </div>
+                <hr />
+                <div class="patient-review">
+                    <h5><strong>Name/Anon from City, State <br />Received X Operation</strong></h5>
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus dui, cursus vitae elit quis, mattis suscipit massa. Fusce nec diam leo."</blockquote>
+                </div>
+                <hr />
+                <ul class="pagination">
+                    <li class="page-item">
+                        <a class="page-link prev" href="#" aria-label="Previous">
+                            <span aria-hidden="true"><i class="bi bi-chevron-double-left"></i></span>
+                        </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true"><i class="bi bi-chevron-double-right"></i></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </main>
+
+<?php
+
+include_once('./partials/footer.php');
+
+?>
